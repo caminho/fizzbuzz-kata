492d677ada8ecbcd0b7d05b043f350933a150d95
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2017-09-08 20:14:52 +0200

    kata 08-09-2017

--- src/main/java/tdd/fizbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizbuzz/FizzBuzz.java
@@ -1,5 +1,39 @@
 package tdd.fizbuzz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
+
+    public String getAnswerFor(int number) {
+        if (isDivisibleByThree(number) && isDivisibleByFive(number)) {
+            return "FizzBuzz";
+        }
+        if (isDivisibleByThree(number)) {
+            return "Fizz";
+        }
+        if (isDivisibleByFive(number)) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
+
+    private boolean isDivisibleByThree(int number) {
+        return number % 3 == 0;
+    }
+
+    private boolean isDivisibleByFive(int number) {
+        return number % 5 == 0;
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        List<String> answers = new ArrayList<>();
+        for (int number = 1; number <= 100; number++) {
+            answers.add(fizzBuzz.getAnswerFor(number));
+        }
+        System.out.println(String.join(",", answers));
+    }
 
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,57 @@
 package tdd.fizzbuzz;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tdd.fizbuzz.FizzBuzz;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTest {
 
+    @Test
+    @Parameters({"1", "2", "4", "7"})
+    public void shouldReturnNumberWhenInputIsNotDivisibleByThreeOrFive(Integer input) {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String answer = fizzBuzz.getAnswerFor(input);
+
+        assertThat(answer).isEqualTo(input.toString());
+    }
+
+    @Test
+    @Parameters({"3", "6", "9", "12", "99"})
+    public void shouldReturnFizzWhenInputIsDivisibleByThreeAndNotByFive(int input) {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String answer = fizzBuzz.getAnswerFor(input);
+
+        assertThat(answer).isEqualTo("Fizz");
+    }
+
+    @Test
+    @Parameters({"5", "10", "20", "25", "35", "40", "100"})
+    public void shouldReturnBuzzWhenInputDivisibleByFiveAndNotByThree(int input) {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String answer = fizzBuzz.getAnswerFor(input);
+
+        assertThat(answer).isEqualTo("Buzz");
+    }
+
+    @Test
+    @Parameters({"15", "30", "45", "60", "75", "90"})
+    public void shouldReturnFizzBuzzWhenInputIsDivisibleByThreeAndFive(int input) {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String answer = fizzBuzz.getAnswerFor(input);
+
+        assertThat(answer).isEqualTo("FizzBuzz");
+    }
 }
