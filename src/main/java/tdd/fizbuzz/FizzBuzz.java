498a646e70068c0cb717d5cd73f50b338766394b
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2017-09-09 07:20:22 +0200

    kata 09-09-2017: printing number for regular input

--- src/main/java/tdd/fizbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizbuzz/FizzBuzz.java
@@ -2,4 +2,7 @@ package tdd.fizbuzz;
 
 public class FizzBuzz {
 
+    public String getAnswerFor(int number) {
+        return String.valueOf(number);
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,26 @@
 package tdd.fizzbuzz;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tdd.fizbuzz.FizzBuzz;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// 7:15
+
+@RunWith(JUnitParamsRunner.class)
 public class FizzBuzzTest {
 
+    @Test
+    @Parameters({"1", "2", "4", "7"})
+    public void shouldPrintNumber(int input) {
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String answer = fizzBuzz.getAnswerFor(input);
+
+        assertThat(answer).isEqualTo(String.valueOf(input));
+    }
 }
